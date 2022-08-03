55ef5e8716dc397c06b6cc2604f44067ddd91917
jiwon22lee <jiwon1799@gmail.com>
2022-08-03 10:45:24 +0900

    dbconntor  ignore

---
+++ src/main/java/com/jw/start/test/DBConnectionTest.java
@@ -0,0 +1,10 @@
+package com.jw.start.test;
+
+public class DBConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/jw/start/test/testMain.java
@@ -0,0 +1,12 @@
+package com.jw.start.test;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("jiwon world");
+
+	}
+
+}
